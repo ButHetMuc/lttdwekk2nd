eb2ddb504ed62e6c9fe76f0df4420c11b95f1562
ButHetMuc <butle132@gmail.com>
2022-09-18 23:20:43 +0700

    week2

--- App.js
+++ App.js
@@ -1,11 +1,32 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button} from 'react-native';
+import { useState } from 'react';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+
+
 
 export default function App() {
+  const [text,setText] = useState('');
+  function clickMe(){
+    console.log(text);
+    
+  }
   return (
+    
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style = {styles.top}>
+        <Text> Hello </Text>
+        <TextInput 
+          placeholder= "typing some texts here" 
+          style= {styles.textInput}
+         ></TextInput>
+        <Button title='click me!' onPress={clickMe()} ></Button>
+      </View>
+      <View style = {styles.bottom}>
+        <Text>I am bottom</Text>
+      </View>
     </View>
   );
 }
@@ -14,7 +35,23 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    
   },
+  top: {
+    flex: 1,
+    justifyContent:'center',
+    alignItems:'center',
+  },
+  bottom:{
+    flex: 1,
+    backgroundColor: 'bluesky',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  textInput:{
+    backgroundColor: 'white',
+    borderWidth:1,
+    width: '90%',
+    height: 48
+  }
 });
